acf11ce9895c214de132b7ae75f0ea75a64b077e
Unrealdst <unrealdst@gmail.com>
2017-02-21 22:36:02 +0100

    remove unnecessary files

--- AwesomeApp/AwesomeApp/App_Start/BundleConfig.cs
+++ AwesomeApp/AwesomeApp/App_Start/BundleConfig.cs
@@ -14,10 +14,6 @@ namespace AwesomeApp
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
-            bundles.Add(new ScriptBundle("~/Index").Include(
-                        "~/Scripts/jqueryIndex.js"));
-
-
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
